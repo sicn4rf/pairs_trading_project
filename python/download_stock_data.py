e8cbaae820c01f57e761997fe6f4436fccc25d52
Francis Padua <paduafrancis45@gmail.com>
2025-06-09 11:33:21 -0700

    Setup tasks

--- python/download_stock_data.py
+++ python/download_stock_data.py
@@ -4,5 +4,9 @@ from datetime import datetime, timedelta
 
 tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'META', 'NVDA', 'TSLA', 'INTC', 'CSCO', 'ORCL']
 
+# Task 1: setup start and end date
+
 end_date = datetime.today()
 start_date = end_date - timedelta(days=365)
+
+# Task 2: iterate through list, and use yfinance to read and pandas to store into csv.
