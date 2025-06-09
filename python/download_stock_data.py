9264da89a7e001edae89e01178467a244f55ab4a
Francis Padua <paduafrancis45@gmail.com>
2025-06-09 11:08:01 -0700

    Forgot to save file before pushing

--- python/download_stock_data.py
+++ python/download_stock_data.py
@@ -1,1 +1,8 @@
 import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'META', 'NVDA', 'TSLA', 'INTC', 'CSCO', 'ORCL']
+
+end_date = datetime.today()
+start_date = end_date - timedelta(days=365)
