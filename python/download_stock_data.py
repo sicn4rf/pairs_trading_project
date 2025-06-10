9930eb1df9ef9cebf8bbe1d9a9d7b3db97960610
Francis Padua <paduafrancis45@gmail.com>
2025-06-09 21:11:08 -0700

    Wrote comments for download_stock_data.py

--- python/download_stock_data.py
+++ python/download_stock_data.py
@@ -2,23 +2,29 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
 
+# This creates a literal list of 10 big tech company tickers that we want to analyze
 tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'META', 'NVDA', 'TSLA', 'INTC', 'CSCO', 'ORCL']
 
-# Task 1: setup start and end date
-
+# Creating two datetime objects using the datetime library. End date being current date
+# and start date being 365 days ago. we do this using .today() and timedelta() functions
 end_date = datetime.today()
 start_date = end_date - timedelta(days=365)
 
-# Task 2: iterate through list, and use yfinance to read and pandas to store into csv.
 
 print("Gathering data from yfinance...")
 
+# Iterate through every ticker in our tickers list
 for tick in tickers:
+    # Create a ticker object for our current company
+    # .Ticker() takes in a string as an argument and returns a ticker object for that particular company
     current_tick = yf.Ticker(tick)
+
+    # Store the stocks historical data into a pandas data frame.
+    # We do this by using the current ticker object to call .history() which returns the OHLCV data
+    # of that particular stock from a start to end date, which we store in a data frame using pandas.
     data_frame = current_tick.history(start=start_date.strftime('%Y-%m-%d'),end=end_date.strftime('%Y-%m-%d'),interval='1d',auto_adjust=True)
 
-    # Task 3: Store into /data folder not python.
-
+    # Use pandas to store the historical data from the data frame into a .csv file
     data_frame.to_csv(f"../data/{tick}.csv")
 
 print("Done collecting data.")
